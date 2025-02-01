203dc48338b16910288cf7d688f3c1ca274326e3
ArtemIsmagilov <118372045+ArtemIsmagilov@users.noreply.github.com>
2025-02-01 19:08:52 +0400

    Update services.rs

--- src/services.rs
+++ src/services.rs
@@ -22,11 +22,11 @@ pub async fn index(req: HttpRequest, mr: web::Json<MergeRequest>) -> impl Respon
         info!("Merge request attributes does not match event: '{}' and action: '{}'. Expected event: merge_request, action: merge", mr.event, mr.action);
         return HttpResponse::PreconditionFailed();
     }
-    let weeek_ids: Vec<_> = RE_WEEEK_TASK_IDS
+    let weeek_ids: Vec<usize> = RE_WEEEK_TASK_IDS
         .captures_iter(&mr.title)
         .map(|c| {
             let (_, [weeek_id]) = c.extract();
-            weeek_id.parse::<usize>().unwrap()
+            weeek_id.parse().unwrap()
         })
         .collect();
     if weeek_ids.is_empty() {
